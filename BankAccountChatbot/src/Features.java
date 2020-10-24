a20f6d306a332d4c9b0c7d58e740d8bb7f66237f
Laya Vadapalli <72660599+layavadapalli@users.noreply.github.com>
2020-10-24 17:22:53 -0500

    Add files via upload

---
+++ BankAccountChatbot/src/Features.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class Features {
+    class Feature
+    {
+        public String[] keywords;
+        public String action;
+
+        public Feature(String[] k, String a)
+        {
+            keywords = k;
+            action = a;
+        }
+    }
+
+    List<Feature> features ;
+
+    public void initialize()
+    {
+        features = new ArrayList<Feature>();
+
+        features.add(new Feature(new String[] {"hi","hello"},"greet"));
+        features.add(new Feature(new String[] {"account", "balance"},"getBalance"));
+        features.add(new Feature(new String[] {"last", "transaction"},"getLastTransaction"));
+        features.add(new Feature(new String[] {"last", "transactions"},"getLastNTransactions"));
+        features.add(new Feature(new String[] {"order", "checkbook", "chequebook"},"orderCheckbook"));
+        features.add(new Feature(new String[] {"cancel", "checkbook", "chequebook","order"},"cancelCheckbook"));
+    }
+
+    public String findMatch(String q1)
+    {
+        q1 = q1.toLowerCase();
+
+        for(int i = 0; i < features.size(); i++)
+        {
+            Feature f = features.get(i);
+
+            int noOfMatches = 0;
+
+            for(int k=0; k < f.keywords.length; k++)
+            {
+                if (q1.contains(f.keywords[k]))
+                    noOfMatches++;
+            }
+
+            if (i == 0 && noOfMatches >= 1)
+                return f.action;
+            else if (noOfMatches >= 2)
+                return f.action;
+        }
+
+        return "";
+    }
+
+}
